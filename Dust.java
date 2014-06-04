1c43e675c5254da76896b7003d87712b56086716
david <david.winiecki@gmail.com>
2014-06-04 08:14:32 -0700

    Version 4.

--- Dust.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Color;
-public class Dust extends Star {
-	protected Dust() {}
-	public Dust(int width, int height) {
-		super(width, height);
-		diam = 3;//(int) (Math.random() * 1) + 2;
-		color = new Color(0xFF808080);
-	}
-}
